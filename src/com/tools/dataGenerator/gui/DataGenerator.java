a09b5c86d9f185eb6da9d349183bdb04a7d38f01
LonghitanoG <giuseppelonghitano@gmail.com>
2019-01-04 15:42:23 +0100

    Removed JTextPane and changed with another JList to display the selected Data Item to be generated. Added new functionalities like: - possibility to remove a subset of items within the selected items

--- src/com/tools/dataGenerator/gui/DataGenerator.java
+++ src/com/tools/dataGenerator/gui/DataGenerator.java
@@ -1,8 +1,6 @@
 package com.tools.dataGenerator.gui;
 
 import javax.swing.*;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.Document;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -22,16 +20,30 @@ public class DataGenerator extends JFrame {
     private JButton mSelectAllBtn;
     private JButton mAddAllBtn;
     private JButton mRemoveBtn;
-    private JList<Object> mProductTypesJList;
-    private JTextPane mSelectedProductTypesTextPane;
+    private JList mAllDataItemsJList;
+    private JList mSelectedDataItemJList;
+    private JPanel mAllDataItemJPane;
+    private JPanel mSelectedDataItemJPane;
+    private JScrollPane mAllDataItemJScrollPane;
+    private JScrollPane mSelectedDataItemScrollJPane;
+
     /**
      * This Map will contain as key the data item name and as value the object representing the data item
      */
-    static private Map<String, DataItem> mProductTypesMap = new TreeMap<>();
+    static private Map<String, DataItem> mDataItemMap = new TreeMap<>();
+
     /**
-     * This list contains all the data item selected from the full list of data item displayed on the left
+     * This list contains the full list of Data Item displayed on the left part of the main Window
      */
-    static private List<String> mSelectedItemsList = new ArrayList<>();
+    static private List<String> mSelectedItemsOnTheLeftList = new ArrayList<>();
+
+    /**
+     * This list contains the list of Data Item displayed on the right part of the main Window that have been
+     * selected. This list is used only to remove the items displayed on the right part of the main window and
+     * that have been selected for deletion
+     */
+    static private List<String> mSelectedItemsOnTheRightList = new ArrayList<>();
+
     static private JFileChooser mFc;
 
 
@@ -42,34 +54,67 @@ public class DataGenerator extends JFrame {
      * Each item is later displayed in the left side of the main window.
      */
     private DataGenerator() {
-        Document doc = mSelectedProductTypesTextPane.getDocument();
-
         readDataItemConfigurationFile();
 
-        setContentDataItemList(mProductTypesJList);
+        setContentDataItemJList(mAllDataItemsJList);
 
-        selectDataItemListener(doc, mAddAllBtn, mProductTypesJList);
+        selectDataItemListener(mAddAllBtn, mAllDataItemsJList, mSelectedDataItemJList);
 
-        removeDataItemListener(doc, mRemoveBtn);
+        selectedDataItemListener(mSelectedDataItemJList, mAddAllBtn);
+
+        removeDataItemListener(mRemoveBtn, mSelectedDataItemJList);
 
         createDataItemListener(mOkBtn);
 
-        selectAllDataItemListener(mSelectAllBtn, mAddAllBtn, mProductTypesJList);
+        selectAllDataItemListener(mSelectAllBtn, mAddAllBtn, mAllDataItemsJList);
 
-        addAllDataItemListener(doc, mAddAllBtn);
+        addAllDataItemListener(mAddAllBtn, mSelectedDataItemJList);
 
-        cancelAllDataItemListener(doc, mCancelBtn, mProductTypesJList, mAddAllBtn);
+        cancelAllDataItemListener(mCancelBtn, mAllDataItemsJList, mAddAllBtn, mSelectedDataItemJList);
+    }
+
+    /**
+     * Method used to keep trace of the Data Item selected on the right JList component.
+     * The selected Data Items are stored into the mSelectedItemsOnTheRightList object.
+     * Later the list is used to remove specific Data Items from the Data Items listed in the right part of the main window
+     *
+     * @param selectedDataItemJList reference to the mSelectedDataItemJList object
+     * @param addAllBtn             reference to the mAddAllBtn object
+     */
+    private static void selectedDataItemListener(JList selectedDataItemJList, JButton addAllBtn) {
+        selectedDataItemJList.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                super.mouseClicked(mouseEvent);
+
+                mSelectedItemsOnTheRightList.clear();
+                JList list = (JList) mouseEvent.getSource();
+
+                addAllBtn.setEnabled(false);
+
+                if (mouseEvent.getClickCount() == 1) {
+                    // one-click detected
+                    if (!list.isSelectionEmpty()) {
+                        String itemName = list.getSelectedValue().toString();
+                        if (!mSelectedItemsOnTheRightList.contains(itemName)) {
+                            mSelectedItemsOnTheRightList.add(itemName);
+                        }
+                    }
+                }
+            }
+        });
     }
 
     /**
      * Method used to set the the JList component with the content read from the
      * configuration file
+     *
      * @param productTypesJList reference to the JList component
      */
-    private static void setContentDataItemList(JList<Object> productTypesJList) {
+    private static void setContentDataItemJList(JList<Object> productTypesJList) {
         // Set content into the ProductTypes JList component
         List<String> itemNames = new ArrayList<>();
-        for (DataItem item : mProductTypesMap.values()) {
+        for (DataItem item : mDataItemMap.values()) {
             itemNames.add(item.getItemName());
         }
         productTypesJList.setListData(itemNames.toArray());
@@ -77,41 +122,36 @@ public class DataGenerator extends JFrame {
 
     /**
      * This method is used to cancel all the Data items added in the JTextPane
-     * @param doc model associated with the editor.
-     * @param cancelBtn reference to the mCancelBtn component
-     * @param productTypesJList reference to the JList component
-     * @param addAllBtn reference to the mAddAllBtn component
+     *
+     * @param cancelBtn             reference to the mCancelBtn component
+     * @param productTypesJList     reference to the JList component
+     * @param addAllBtn             reference to the mAddAllBtn component
+     * @param selectedDataItemJList reference to the mSelectedDataItemJList
      */
-    private static void cancelAllDataItemListener(Document doc, JButton cancelBtn, JList<Object> productTypesJList, JButton addAllBtn) {
+    private static void cancelAllDataItemListener(JButton cancelBtn, JList productTypesJList, JButton addAllBtn, JList selectedDataItemJList) {
         cancelBtn.addActionListener(actionEvent -> {
             productTypesJList.clearSelection();
             addAllBtn.setEnabled(false);
-            if (!mSelectedItemsList.isEmpty()) {
-                try {
-                    doc.remove(0, doc.getLength());
-                    mSelectedItemsList.clear();
-                } catch (BadLocationException e) {
-                    e.printStackTrace();
-                }
+            if (!mSelectedItemsOnTheLeftList.isEmpty()) {
+                mSelectedItemsOnTheLeftList.clear();
+
+                selectedDataItemJList.setListData(mSelectedItemsOnTheLeftList.toArray());
             }
         });
     }
 
     /**
      * This method is used to add all the Data Item to the JTextPane once a;ll the items have been selected
-     * @param doc model associsated with the editor
-     * @param addAllBtn reference to the mAddAllBtn component
+     *
+     * @param addAllBtn             reference to the mAddAllBtn objec
+     * @param selectedDataItemJList reference to the mSelectedDataItemJList object
      */
-    private static void addAllDataItemListener(Document doc, JButton addAllBtn) {
+    private static void addAllDataItemListener(JButton addAllBtn, JList selectedDataItemJList) {
         addAllBtn.addActionListener(actionEvent -> {
-            for (Map.Entry<String, DataItem> entry : mProductTypesMap.entrySet()) {
-                if (!mSelectedItemsList.contains(entry.getKey())) {
-                    mSelectedItemsList.add(entry.getKey());
-                    try {
-                        doc.insertString(doc.getLength(), entry.getKey() + "\n", null);
-                    } catch (BadLocationException e) {
-                        e.printStackTrace();
-                    }
+            for (Map.Entry<String, DataItem> entry : mDataItemMap.entrySet()) {
+                if (!mSelectedItemsOnTheLeftList.contains(entry.getKey())) {
+                    mSelectedItemsOnTheLeftList.add(entry.getKey());
+                    selectedDataItemJList.setListData(mSelectedItemsOnTheLeftList.toArray());
                 }
             }
         });
@@ -120,15 +160,15 @@ public class DataGenerator extends JFrame {
     /**
      * This method is used to select all the Data items listed in the JList component
      *
-     * @param selectAllBtn reference to the mSelectAllBtn component
-     * @param productTypesJList reference to the JList component
+     * @param selectAllBtn      reference to the mSelectAllBtn cbject
+     * @param addAllBtn         reference to the mAddAllBtn object
+     * @param productTypesJList reference to the JList object
      */
-    private static void selectAllDataItemListener(JButton selectAllBtn, JButton addAllBtn, JList<Object> productTypesJList) {
+    private static void selectAllDataItemListener(JButton selectAllBtn, JButton addAllBtn, JList productTypesJList) {
         selectAllBtn.addActionListener(actionEvent -> {
-            int[] indecis = new int[mProductTypesMap.size()];
-            for (int i = 0; i < mProductTypesMap.size(); i++) {
+            int[] indecis = new int[mDataItemMap.size()];
+            for (int i = 0; i < mDataItemMap.size(); i++) {
                 indecis[i] = i;
-
             }
             productTypesJList.setSelectedIndices(indecis);
             addAllBtn.setEnabled(true);
@@ -138,20 +178,21 @@ public class DataGenerator extends JFrame {
     /**
      * Iterate the list of the selected data item displayed in the JTextPane
      * and for each data item create a file
-     * @param okBtn reference to the mOkBtn component
+     *
+     * @param okBtn reference to the mOkBtn object
      */
     private void createDataItemListener(JButton okBtn) {
         okBtn.addActionListener(actionEvent -> {
             //Create a Data Item for each element displayed in the JTextPane
-            if (!mSelectedItemsList.isEmpty()) {
+            if (!mSelectedItemsOnTheLeftList.isEmpty()) {
                 //Create a file chooser
                 mFc = new JFileChooser();
                 mFc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
                 int ret = mFc.showSaveDialog(DataGenerator.this);
                 if (ret == JFileChooser.APPROVE_OPTION) {
                     File selectedPath = mFc.getSelectedFile();
-                    for (String s : mSelectedItemsList) {
-                        DataItem item = mProductTypesMap.get(s);
+                    for (String s : mSelectedItemsOnTheLeftList) {
+                        DataItem item = mDataItemMap.get(s);
                         System.out.println(item.getItemName() + " " + item.getDimesion() + " MB");
 
                         RandomAccessFile file;
@@ -165,43 +206,50 @@ public class DataGenerator extends JFrame {
                     }
                 }
             }
+            JOptionPane.showMessageDialog(this, "Data Items have been created!");
         });
     }
 
     /**
-     * Method used to remove from the JTextPane the last data item inserted
-     * @param doc model associated with the editor.
+     * Method used to remove from the JList on the right part of the main window either the last Data Item inserted
+     * or the item selected
+     *
+     * @param removeBtn             reference to the mRemoveBtn objetc
+     * @param selectedDataItemJList reference to the mSelectedDataItemJLis object
      */
-    private static void removeDataItemListener(Document doc, JButton removeBtn) {
-        // This listener is use to remove from the JtextPane the last item
+    private static void removeDataItemListener(JButton removeBtn, JList selectedDataItemJList) {
+        // This listener is use to remove from the right JList either the selected items or the last item
         // added in the list
         removeBtn.addActionListener(actionEvent -> {
-            //Remove last selected product type from the Selected item text pane
-            if (!mSelectedItemsList.isEmpty()) {
-                try {
-                    doc.remove(0, doc.getLength());
-                    mSelectedItemsList.remove(mSelectedItemsList.size() - 1);
-
-                    for (String s : mSelectedItemsList) {
-                        doc.insertString(doc.getLength(), s + "\n", null);
-                    }
-                } catch (BadLocationException e) {
-                    e.printStackTrace();
+            //Remove the selected items
+            if (!mSelectedItemsOnTheRightList.isEmpty()) {
+                for (String item : mSelectedItemsOnTheRightList) {
+                    mSelectedItemsOnTheLeftList.remove(item);
                 }
+                mSelectedItemsOnTheRightList.clear();
+                selectedDataItemJList.setListData(mSelectedItemsOnTheLeftList.toArray());
+            }
+            //Remove last selected data item from the right JList
+            else if (!mSelectedItemsOnTheLeftList.isEmpty()) {
+                mSelectedItemsOnTheLeftList.remove(mSelectedItemsOnTheLeftList.size() - 1);
+                selectedDataItemJList.setListData(mSelectedItemsOnTheLeftList.toArray());
             }
         });
     }
 
     /**
-     * Method defines a Mouse Listener for the JList element that contains all the data items
-     * read from the configuration file.
+     * Method defines a Mouse Listener for the JList element that contains all the Data Items
+     * read from the configuration file. The Data Item list is displayed on the right part of the
+     * main windows
      *
-     * @param doc model associated with the editor.
+     * @param addAllBtn             reference to mAddAllBtn object
+     * @param allDataItemsJList     reference to the JList containing all the Data Items read from the configuration file
+     * @param selectedDataItemJList reference to the JList containing the selected Data Items that will be generated
      */
-    private static void selectDataItemListener(Document doc, JButton addAllBtn, JList<Object> productTypesJList) {
+    private static void selectDataItemListener(JButton addAllBtn, JList allDataItemsJList, JList selectedDataItemJList) {
         // Here when an item in the JList is clicked, it will be added in the
         // JTextPane on the right
-        productTypesJList.addMouseListener(new MouseAdapter() {
+        allDataItemsJList.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent mouseEvent) {
                 super.mouseClicked(mouseEvent);
@@ -213,14 +261,9 @@ public class DataGenerator extends JFrame {
                     // Double-click detected
                     String itemName = list.getModel().getElementAt(list.locationToIndex(mouseEvent.getPoint())).toString();
 
-                    if (!mSelectedItemsList.contains(itemName)) {
-                        mSelectedItemsList.add(itemName);
-
-                        try {
-                            doc.insertString(doc.getLength(), itemName + "\n", null);
-                        } catch (BadLocationException e) {
-                            e.printStackTrace();
-                        }
+                    if (!mSelectedItemsOnTheLeftList.contains(itemName)) {
+                        mSelectedItemsOnTheLeftList.add(itemName);
+                        selectedDataItemJList.setListData(mSelectedItemsOnTheLeftList.toArray());
                     }
                 }
             }
@@ -237,7 +280,7 @@ public class DataGenerator extends JFrame {
                 // split the String line (Ex: P4_1A_HR_____:10) into two strings. The first one containing the
                 // data item name and the second one the dimension
                 String[] tmp = line.split(":");
-                mProductTypesMap.put(tmp[0], new DataItem(tmp[0], Double.valueOf(tmp[1])));
+                mDataItemMap.put(tmp[0], new DataItem(tmp[0], Double.valueOf(tmp[1])));
             });
         } catch (IOException e) {
             e.printStackTrace();
@@ -248,7 +291,7 @@ public class DataGenerator extends JFrame {
     public static void main(String[] args) {
         JFrame frame = new JFrame("DataGenerator");
         frame.setContentPane(new DataGenerator().mMainPane);
-        frame.setPreferredSize(new Dimension(800, 500));
+        frame.setPreferredSize(new Dimension(1500, 500));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
